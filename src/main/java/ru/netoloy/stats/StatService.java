1edb0d28b72361575f8dc1217e07b8b6cdb3c52e
Альфия Устименко <alfiya.ustimenko@ukr.net>
2022-04-16 23:54:04 +0500

    Initial commit2

--- src/main/java/ru/netoloy/stats/StatService.java
+++ src/main/java/ru/netoloy/stats/StatService.java
@@ -2,11 +2,70 @@ package ru.netoloy.stats;
 
 public class StatService {
 
-    public int calculateSum(int[]sales){
+    public int calculateSum(int[] sales) {
         int sum = 0;
-        for (int sale : sales){
+        for (int sale : sales) {
             sum += sale;
         }
         return sum;
     }
+
+    public int calculateMiddle(int[] sales) {
+        return calculateSum(sales) / sales.length;
+    }
+    public int maxSales(int[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (int sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale >= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+        public int minSales(int[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (int sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+    public int calculateAmountBelow(int[] sales) {
+        int amount = 0;
+        int averageSale = calculateMiddle(sales);
+        for (int sale : sales) {
+            if (sale < averageSale) {
+                amount++;
+            }
+        }
+        return amount;
+    }
+    public int calculateAmountHigher(int[] sales) {
+        int amount = 0;
+        int averageSale = calculateMiddle(sales);
+        for (int sale : sales) {
+            if (sale > averageSale) {
+                amount++;
+            }
+        }
+        return amount;
+    }
+
+
+
+
+
+
+
+
 }
--- src/test/java/ru/netoloy/stats/StatServiceTest.java
+++ src/test/java/ru/netoloy/stats/StatServiceTest.java
@@ -9,9 +9,58 @@ class StatServiceTest {
     @Test
     void shouldCalculateSum() {
         StatService service = new StatService();
-    int[] sals = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-    int expected  = 180;
-    int act = service.calculateSum(sale);
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.calculateSum(sales);
+        assertEquals(expected, actual);
 
+    }
+
+    @Test
+    void shouldCalculateMiddleSum() {
+        StatService service = new StatService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.calculateMiddle(sales);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldMaxSales() {
+        StatService service = new StatService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.maxSales(sales);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldMinSales() {
+        StatService service = new StatService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.minSales(sales);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateAmountBelow() {
+        StatService service = new StatService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.calculateAmountBelow(sales);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void shouldCalculateAmountHigher() {
+        StatService service = new StatService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.calculateAmountHigher(sales);
+        assertEquals(expected, actual);
     }
 }
